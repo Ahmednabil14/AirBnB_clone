9975c1ada71f4bbcd783e13ee73e78cf554bf318
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-01-14 21:44:52 +0200

    test

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,8 @@
 """Create an instance of FileStorage and Reload data
 from the JSON file (if any)
 """
-
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
