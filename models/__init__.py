ef8db363af6544496095a7896fd4ea3ee409281c
Mai <mai.zakaria.18@gmail.com>
2024-01-14 10:21:02 -0800

    test

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 """Create an instance of FileStorage and Reload data
 from the JSON file (if any)
 """
+
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
